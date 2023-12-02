6612629d9dd300ecfd3c6713bac58ba0ad1128c6
tdxa <anndoman3@gmail.com>
2023-12-02 21:11:24 +0100

    :sparkles: Solve part two of day 1

--- years/2023/day-1/day1.test.ts
+++ years/2023/day-1/day1.test.ts
@@ -1,22 +1,39 @@
 import { sumStringArray } from "../../../helpers/array";
-import { recoverCalibrationValues } from "./day1";
+import { DIGIT_NAMES_MAP, recoverCalibrationValues } from "./day1";
 
-describe('Recover Calibration Values', () => {
-  it('should return expected calibration values', () => {
-    const lines = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'];
-    const expectedCalibrationValues = ['12', '38', '15', '77'];
+describe("Recover Calibration Values - part 1", () => {
+  it("should return expected calibration values", () => {
+    const lines = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+    const expectedCalibrationValues = ["12", "38", "15", "77"];
 
     const calibrationValues = recoverCalibrationValues(lines);
 
     expect(calibrationValues).toEqual(expectedCalibrationValues);
   });
-  
-  it('should return expected calibration sum', () => {
-    const calibrationValues = ['12', '38', '15', '77'];
+
+  it("should return expected calibration sum", () => {
+    const calibrationValues = ["12", "38", "15", "77"];
     const expectedCalibrationSum = 142;
 
     const calibrationSum = sumStringArray(calibrationValues);
 
     expect(calibrationSum).toEqual(expectedCalibrationSum);
+  });
+});
+
+describe("Recover Calibration Values - part 2", () => {
+  it("Replaces overlapping combinations correctly", () => {
+    const testCases: Array<Array<string>> = [
+      ["xtwonex", "xt2o1ex"],
+      ["threeightoep", "t3e8toep"],
+    ];
+
+    testCases.forEach(([input, expectedOutput]) => {
+      const replaced = Object.entries(DIGIT_NAMES_MAP).reduce(
+        (acc, [key, value]) => acc.replace(new RegExp(key, "g"), value),
+        input
+      );
+      expect(replaced).toEqual(expectedOutput);
+    });
   });
 });
--- years/2023/day-1/day1.ts
+++ years/2023/day-1/day1.ts
@@ -1,10 +1,22 @@
 import { sumStringArray } from "../../../helpers/array";
 import { readFile } from "../../../helpers/files";
-import {WINDOWS_NEWLINE} from "../../../constants/index";
+import { WINDOWS_NEWLINE } from "../../../constants/index";
 
 const INPUT_FILE = __dirname + "/input.txt";
 
 const lines = readFile(INPUT_FILE).split(WINDOWS_NEWLINE)
+
+export const DIGIT_NAMES_MAP: Record<string, string> = {
+  one: 'o1e',
+  two: 't2o',
+  three: 't3e',
+  four: 'f4r',
+  five: 'f5e',
+  six: 's6x',
+  seven: 's7n',
+  eight: 'e8t',
+  nine: 'n9e',
+};
 
 export const recoverCalibrationValues = (lines: Array<string>): Array<string> => {
     const linesDigits = lines.map(line => line.match(/\d/g)) as  Array<Array<string>>
@@ -12,9 +24,16 @@ export const recoverCalibrationValues = (lines: Array<string>): Array<string> =>
     return calibrationValues
 }
 
+export const replacedWords: string[] = lines.map(word => {
+  Object.entries(DIGIT_NAMES_MAP).forEach(([key, value]) => {
+    const regex = new RegExp(key, 'g');
+    word = word.replace(regex, value);
+  });
+  return word;
+});
 
-const calibrationSum = sumStringArray(recoverCalibrationValues(lines))
 
+const calibrationSum = sumStringArray(recoverCalibrationValues(replacedWords))
 
 console.log(calibrationSum)
 
