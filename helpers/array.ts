86e48f65957fbae00f815c0f3add4868324d106f
tdxa <anndoman3@gmail.com>
2024-12-01 21:54:34 +0100

    :sparkles: solve part two of day 1

--- helpers/array.ts
+++ helpers/array.ts
@@ -5,3 +5,20 @@ export const sumArray = (array: Array<string | number>): number =>
 
 export const multiplyAllElementsArray = (array: Array<number>): number =>
   array.reduce((a, b) => a * b);
+
+/**
+ * Counts the occurrences of a specified value in an array.
+ *
+ * @param {Array<number>} arr - The array of numbers to search.
+ * @param {number} value - The value to count occurrences of.
+ * @returns {number} The number of times `value` appears in `arr`.
+ *
+ * @example
+ * const arr = [1, 2, 3, 2, 4, 2];
+ * const value = 2;
+ *
+ * const occurrences = count(arr, value);
+ * Output: 3
+ */
+export const getValueFrequency = (arr: Array<number>, value: number) =>
+  arr.filter((val) => val === value).length;
--- years/2024/day-1/day1.ts
+++ years/2024/day-1/day1.ts
@@ -1,5 +1,6 @@
 import { WINDOWS_NEWLINE } from "../../../constants";
 import { REGEX_SPACE_ONE_OR_MORE } from "../../../constants/regex";
+import { getValueFrequency } from "../../../helpers/array";
 import { readFile } from "../../../helpers/files";
 
 const INPUT_FILE = __dirname + "/input.txt";
@@ -15,12 +16,14 @@ lines.forEach((line) => {
   rightColumn.push(right);
 });
 
+// PART ONE
+
 const sortedLeftColumn: Array<number> = leftColumn.sort();
 const sortedRightColumn: Array<number> = rightColumn.sort();
 
 const calculateDistance = (
-  leftColumn: number[],
-  rightColumn: number[]
+  leftColumn: Array<number>,
+  rightColumn: Array<number>
 ): number =>
   leftColumn.reduce(
     (sum, leftValue, i) => sum + Math.abs(leftValue - rightColumn[i]),
@@ -28,3 +31,16 @@ const calculateDistance = (
   );
 
 console.log(calculateDistance(sortedLeftColumn, sortedRightColumn));
+
+// PART TWO
+
+const calculateSimilarityScore = (
+  leftColumn: Array<number>,
+  rightColumn: Array<number>
+) =>
+  leftColumn.reduce(
+    (sum, value) => sum + value * getValueFrequency(rightColumn, value),
+    0
+  );
+
+console.log(calculateSimilarityScore(sortedLeftColumn, sortedRightColumn));
